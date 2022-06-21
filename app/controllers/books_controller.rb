aec5c2866d53df8d8b7a6569ec091f47dd576f8a
EC2 Default User <ec2-user@ip-172-31-84-149.ec2.internal>
2022-06-21 04:39:02 +0000

    [Add]投稿フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
+  def new
+  end
+
   def index
+    @list = List.new
+    @lists = List.all
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/books/index'
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end

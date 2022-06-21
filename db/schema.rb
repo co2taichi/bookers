334ae64da69310c70789bfb665b12ed170a22d4b
EC2 Default User <ec2-user@ip-172-31-84-149.ec2.internal>
2022-06-21 00:23:01 +0000

    [Add]controller_homes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2022_06_21_000344) do
 
   create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
